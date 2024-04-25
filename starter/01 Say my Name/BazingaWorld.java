6ea731174153fa355412e328f2935884f4b0feae
Leticia Torres Ramirez <leticiat@microsoft.com>
2024-04-25 13:36:49 -0600

    feat: exercise 1

---
+++ starter/01 Say my Name/BazingaWorld.java
@@ -0,0 +1,10 @@
+//I need a java function to print Bazinga World!!!
+public class BazingaWorld {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("Bazinga World!!!");
+    }
+}
